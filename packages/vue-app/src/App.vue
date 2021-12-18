513933075166b191fddbeb1cf1e42ff85e52c33a
chen jiajing <16602132375@163.com>
2021-12-18 15:29:24 +0800

    feat: init

--- packages/vue-app/src/App.vue
+++ packages/vue-app/src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <!-- 使用 element 菜单栏 -->
+    <el-menu :router="true" mode="horizontal">
+      <!-- 基座中可以放自己的路由 -->
+      <el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/about">About</el-menu-item>
+      <!-- 也可以引用其它子应用 -->
+      <el-menu-item index="/vue">vue 应用</el-menu-item>
+      <el-menu-item index="/react">react 应用</el-menu-item>
+    </el-menu>
+    <!-- 页面渲染 -->
+    <router-view></router-view>
+    <div id="vue-app"></div>
+    <div id="react-app"></div>
+  </div>
 </template>
 
 <style lang="less">
